d1a1b66a50f6d4f86e8b7fa49f4fbfe4e6b73660
khsyukatu <higuchi@unistyleinc.com>
2015-09-08 01:23:34 +0000

    age added

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907061125) do
+ActiveRecord::Schema.define(version: 20150908011505) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
